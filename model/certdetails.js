d2b15c39f6c97e2d8dadfa3258cb53395eb92633
Mrudhula2102 <mrudhulamohandas@gmail.com>
2023-12-04 12:05:03 +0530

    first commit

---
+++ model/certdetails.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://Mrudhula2102:mahadev5321@cluster0.c5zoall.mongodb.net/db1?retryWrites=true&w=majority")
+.then(()=>{console.log("DB connected")})
+.catch(err=>console.log(err));
+
+let sc=mongoose.Schema;
+const certschema = new sc({
+    sid:{type:mongoose.Schema.Types.ObjectId,ref:'students'},
+    qualification:String,
+    certphoto:{
+        data:Buffer,
+        contentType:String
+    }
+});
+var certmodel=mongoose.model("certificate",certschema)
+module.exports=certmodel;
